008f5c6b80fbb76abbdc866b546b99e425fb8bac
Evgeni Dinev <ewgeni.dinew@gmail.com>
2018-05-07 21:30:29 +0300

    --added solution for first lecture problems

---
+++ 01.SortingAndSearching/02.QuickSort/Launcher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.QuickSort
+{
+    class Launcher
+    {
+        static void Main(string[] args)
+        {
+            var array = Console.ReadLine()
+               .Split(' ')
+               .Select(int.Parse)
+               .ToArray();
+
+        }
+
+        private static void Sort(int[] array)
+        {
+            var left = 0;
+            var right = array.Length-1;
+            var middle = array.Length / 2;
+
+            if (array.Length<3)
+            {
+                return;
+            }
+            var element = array[middle];
+
+            for (int i = left; i <right; i++)
+            {
+                if (array[i]<element)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ 01.SortingAndSearching/11.EightQueens/Launcher.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Launcher
+    {
+        static char[][] field = new char[8][];
+
+        static void Main(string[] args)
+        {
+            InitializeField();
+            if (CheckQueensMethod(0, 0, 1))
+            {
+                PrintField();
+            }
+        }
+
+        private static bool CheckQueensMethod(int x, int y, int move)
+        {
+            field[x][y] = 'Q';
+
+            if (move >= 8)
+            {
+                return true;
+            }
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (CanPlaceQueen(i, j))
+                    {
+                        var flag = CheckQueensMethod(i, j, move + 1);
+
+                        if (flag)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            field[x][y] = '-';
+            return false;
+        }
+
+        private static bool CanPlaceQueen(int x, int y)
+        {
+            if (field[x][y] == 'Q')
+            {
+                return false;
+            }
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (field[i][y] == 'Q' || field[x][i] == 'Q')
+                {
+                    return false;
+                }
+                else if (!DiagonalContainsQueen(x, y))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool DiagonalContainsQueen(int row, int col)
+        {
+            for (int i = row - 1, j = col + 1; i >= 0 && j < 8; i--, j++)
+            {
+                if (field[i][j] == 'Q')
+                {
+                    return false;
+                }
+            }
+            for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+            {
+                if (field[i][j] == 'Q')
+                {
+                    return false;
+                }
+            }
+            for (int i = row + 1, j = col - 1; i < 8 && j >= 0; i++, j--)
+            {
+                if (field[i][j] == 'Q')
+                {
+                    return false;
+                }
+            }
+            for (int i = row + 1, j = col + 1; i < 8 && j < 8; i++, j++)
+            {
+                if (field[i][j] == 'Q')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static void PrintField()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine(string.Join("", field[i]));
+            }
+            Console.WriteLine();
+
+        }
+
+        private static void InitializeField()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                field[i] = "--------".ToCharArray();
+            }
+        }
+
+    }
+}

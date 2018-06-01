4d7a4f43743e7c140cee0f1d3ec7c003d49920f0
Evgeni Dinev <ewgeni.dinew@gmail.com>
2018-06-01 11:17:24 +0300

    --added university simple task solution

---
+++ Diamonds/Diamonds/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace Diamonds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = new Stopwatch();
+
+            var input = int.Parse(Console.ReadLine());
+            long sum = 0;
+
+            watch.Start();
+
+            for (int i = 0; i <= input; i++)
+            {
+                for (int j = i; j <= input; j++)
+                {
+                    sum += (j + i);
+                }
+            }
+            watch.Stop();
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("Time: " + watch.ElapsedMilliseconds);
+        }
+    }
+}
